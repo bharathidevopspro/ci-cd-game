828a1e0466ca956ddfdf57c788fdd4b4b344033c
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-03 14:22:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,8 @@ resource "aws_instance" "game_server" {
   instance_type = "t3.small"
   key_name      = "new"
 
-  security_groups = [aws_security_group.bharathigame.name]
+    vpc_security_group_ids = [aws_security_group.bharathigame.id]
+
 
   user_data = <<-EOF
               #!/bin/bash
