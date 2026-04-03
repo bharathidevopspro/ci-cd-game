f6eb9010439840599fa158035ed7e8b37efd9085
bharathidevopspro <bharathi.devops.pro@gmail.com>
2026-04-03 13:34:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "game_server" {
   ami           = "ami-0ec10929233384c7f" 
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   key_name      = "ubuntu"
 
   security_groups = [aws_security_group.gamesathe.name]
